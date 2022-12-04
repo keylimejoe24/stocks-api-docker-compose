110714983649eb2947227af6229765ae377c4f4b
Joe Jackson <joseph.jackson@inturn.co>
2022-12-04 14:37:32 -0800

    fix

--- run-scrape-services.sh
+++ run-scrape-services.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-rm -rf stocks-api-docker-compose
-git clone https://github.com/keylimejoe24/stocks-api-docker-compose.git
-cd stocks-api-docker-compose
 DB_HOST=$1 docker-compose up scraping-server -d
