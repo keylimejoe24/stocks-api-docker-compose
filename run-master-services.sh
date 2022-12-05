47f27d4f46907d2507604f43b336924b866ed378
Joe Jackson <joseph.jackson@inturn.co>
2022-12-04 21:21:00 -0800

    start scripts

---
+++ run-master-services.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker-compose up mongodb prometheus grafana algorithms-server --build -d
--- run-scrape-services.sh
+++ run-scrape-services.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-DB_HOST=$1 docker-compose up -f docker-compose-scraping-server.yml -d
+DB_HOST=$1 docker-compose up scraping-server --build -d
+
+ # DB_HOST=11111 docker-compose up scraping-server --build -d & exit".format(instances[0].public_ip_address)
+    # "docker-compose up mongodb prometheus grafana algorithms-server --build -d &>/dev/null"
