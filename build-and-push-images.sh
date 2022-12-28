9b913e6913b6b327f51025f18220f6200b7f7081
Favorite Child <favoritechild@Loras-MacBook-Air.local>
2022-12-28 12:59:21 -0700

    added cors to flask backend

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -3,5 +3,3 @@ DB_HOST=$1 docker-compose build --build-arg ACCESS_KEY=$2 --build-arg SECRET_ACC
 docker push joja5627/node-server:latest
 docker push joja5627/frontend:latest
 docker push joja5627/boto3-flask:latest
-
-
