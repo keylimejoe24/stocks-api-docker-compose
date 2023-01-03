efa3c02eb778e28e4251986980c3ff772c820b80
favoritechild <favoritechild@Loras-MacBook-Air.local>
2023-01-03 13:53:13 -0700

    generated prometheus config

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose build --build-arg MASTER_IP=$1 --build-arg ACCESS_KEY=$2 --build-arg SECRET_ACCESS_KEY=$3 --build-arg DEPLOYMENT_VERSION=$4
-docker push joja5627/node-server:latest
-docker push joja5627/frontend:latest
+# docker push joja5627/node-server:latest
+# docker push joja5627/frontend:latest
 docker push joja5627/boto3-flask:latest
 
--- python3-boto-env/app/app.py
+++ python3-boto-env/app/app.py
@@ -43,12 +43,12 @@ def health_check():
     
      
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
-    return response
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+#     return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
