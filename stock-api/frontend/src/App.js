f1f2ec96bc067f50f5bc2c638bf80e79b9a8e349
Joe Jackson <joseph.jackson@inturn.co>
2023-01-15 13:31:38 -0700

    generated prometheus config

--- stock-api/frontend/src/App.js
+++ stock-api/frontend/src/App.js
@@ -27,7 +27,7 @@ socketIOConfig.map(url => {
   sockets.push(newSocket)
 })
 
-const MASTER_IP = "3.91.106.214"
+const MASTER_IP = "3.85.193.26"
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: '#fafafa',
   ...theme.typography.body2,
