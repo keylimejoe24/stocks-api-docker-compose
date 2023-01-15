4fca7be81531adb5fb00e1d5338068ce25ce291f
Joe Jackson <joseph.jackson@inturn.co>
2023-01-15 14:12:02 -0700

    generated prometheus config

--- python3-boto-env/scripts/refresh_ec2_instances.py
+++ python3-boto-env/scripts/refresh_ec2_instances.py
@@ -180,12 +180,12 @@ wait_for_services_to_start([master_instances[0]], ["http://{}:3002/api/health", 
 
 commands = [
     "sudo su",
-    "cd /home/ssm-user",
+    "cd /home/ssm-user/stocks-api-docker-compose",
     # "git clone https://github.com/keylimejoe24/stocks-api-docker-compose.git",
-    "cd stocks-api-docker-compose",
+    
     "docker-compose down",
     "docker system prune --filter  'until=5h' -f"
-    "git pull --no-edit origin main",
+    "git pull",
     # "docker login --username joja5627 --password-stdin < my_password.txt",  
     "DB_HOST={} docker-compose up --build -d scraping-server".format(master_instances[0]["public_ip_address"])
 ]
